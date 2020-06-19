841e2fc7fb35d17990d44f99639fcf39e7accc14
Parshant Sharma <parshantsharma360@gmail.com>
2020-06-19 21:14:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,18 @@
 import os, youtube_dl
-title = os.environ['TITLE']
-print(title)
+
+video_url = os.environ['TITLE']
+video_info = youtube_dl.YoutubeDL().extract_info(url=video_url, download=False)
+
+filename = f"{video_info['title']}.mp3"
+options = {
+    'format': 'bestaudio/best',
+    'keepvideo': False,
+    'outtmpl': filename,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]
+}
+
+youtube_dl.YoutubeDL(options).download([video_info['webpage_url']])
